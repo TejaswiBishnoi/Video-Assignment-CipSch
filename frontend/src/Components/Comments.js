b5a73364ce1d69fc6cf01f097a63389758d270d8
Tejaswi Bishnoi <62018605+TejaswiBishnoi@users.noreply.github.com>
2023-03-13 12:24:48 +0530

    Comment Component - lower part

--- frontend/src/Components/Comments.js
+++ frontend/src/Components/Comments.js
@@ -1,24 +1,28 @@
-import { Box, Paper, Stack, Typography} from "@mui/material";
+import { Box, Paper, Stack, TextField, TextareaAutosize, Typography} from "@mui/material";
 import React from "react";
 
 function Comments(props){
     return(
-        <Stack direction={'column'} justifyContent={'space-between'} spacing={2}>
-            <Paper sx={{height:'110vh', overflowY:'scroll'}}>
-                <Typography fontSize={40}>Hello</Typography>
-                <Typography fontSize={40}>Hello</Typography>
-                <Typography fontSize={40}>Hello</Typography>
-                <Typography fontSize={40}>Hello</Typography>
-                <Typography fontSize={40}>Hello</Typography>
-                <Typography fontSize={40}>Hello</Typography>
-                <Typography fontSize={40}>Hello</Typography>
-                <Typography fontSize={40}>Hello</Typography>
-                <Typography fontSize={40}>Hello</Typography>
-                <Typography fontSize={40}>Hello</Typography>
-                <Typography fontSize={40}>Hello</Typography>
-                <Typography fontSize={40}>Hello</Typography>
+        <Stack height={'90vh'} direction={'column'} justifyContent={'space-between'} spacing={2} my={2} mx={2}>
+            <Paper sx={{height:'60vh', overflowY:'scroll', display:"flex", flexGrow: 1}}>
+                <Box>
+                    <Typography fontSize={40}>Hello</Typography>
+                    <Typography fontSize={40}>Hello</Typography>
+                    <Typography fontSize={40}>Hello</Typography>
+                    <Typography fontSize={40}>Hello</Typography>
+                    <Typography fontSize={40}>Hello</Typography>
+                    <Typography fontSize={40}>Hello</Typography>
+                    <Typography fontSize={40}>Hello</Typography>
+                    <Typography fontSize={40}>Hello</Typography>
+                    <Typography fontSize={40}>Hello</Typography>
+                    <Typography fontSize={40}>Hello</Typography>
+                    <Typography fontSize={40}>Hello</Typography>
+                    <Typography fontSize={40}>Hello</Typography>
+                </Box>
             </Paper>
-            <Paper sx={{height:'10vh'}}></Paper>
+            <Paper sx={{height:100, backgroundColor:'whitesmoke'}}>
+                <TextField multiline rows={2} color="success" sx={{backgroundColor:"white", display:"flex", flexGrow: 1}}/>
+            </Paper>
         </Stack>
     );
 }
