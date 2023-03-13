34c57ad36833f06fcde06fa264614495771c773a
Tejaswi Bishnoi <62018605+TejaswiBishnoi@users.noreply.github.com>
2023-03-13 17:45:53 +0530

    Comment Cards, comment box complete

---
+++ frontend/src/Components/CommentCard.js
@@ -0,0 +1,28 @@
+import { Button, Card, CardContent, CardHeader, Stack, Typography } from "@mui/material";
+import React from "react";
+
+function CommentCard(props){
+    return(
+        <Card>
+            <CardContent sx={{pt: 0.5, pb: 0.5, '&:last-child': { pb: 0 }}}>
+                <Stack direction={'row'} justifyContent={'space-between'}>
+                    <Typography fontSize={'0.7rem'} color={'gray'}>
+                        {props.data.author}
+                    </Typography>
+                    <Typography fontSize={'0.7rem'} color={'gray'}>
+                        {props.data.date}
+                    </Typography> 
+                </Stack>
+                <Typography align="justify" fontSize={'0.85rem'}>
+                    {props.data.content}
+                </Typography>
+                <Stack direction={'row'} justifyContent={'left'}>
+                    <Button sx={{textTransform: 'capitalize', fontSize:'0.7rem'}}>
+                        {props.data.replies} Replies
+                    </Button>
+                </Stack>
+            </CardContent>
+        </Card>
+    );
+}
+export default CommentCard;
--- frontend/src/Components/Comments.js
+++ frontend/src/Components/Comments.js
@@ -1,27 +1,26 @@
-import { Box, Paper, Stack, TextField, TextareaAutosize, Typography} from "@mui/material";
+import { Box, Button, Paper, Stack, TextField, TextareaAutosize, Typography} from "@mui/material";
 import React from "react";
+import SendIcon from '@mui/icons-material/Send';
+import CommentCard from "./CommentCard";
 
 function Comments(props){
     return(
-        <Stack height={'90vh'} direction={'column'} justifyContent={'space-between'} spacing={2} my={2} mx={2}>
-            <Paper sx={{height:'60vh', overflowY:'scroll', display:"flex", flexGrow: 1}}>
-                <Box>
-                    <Typography fontSize={40}>Hello</Typography>
-                    <Typography fontSize={40}>Hello</Typography>
-                    <Typography fontSize={40}>Hello</Typography>
-                    <Typography fontSize={40}>Hello</Typography>
-                    <Typography fontSize={40}>Hello</Typography>
-                    <Typography fontSize={40}>Hello</Typography>
-                    <Typography fontSize={40}>Hello</Typography>
-                    <Typography fontSize={40}>Hello</Typography>
-                    <Typography fontSize={40}>Hello</Typography>
-                    <Typography fontSize={40}>Hello</Typography>
-                    <Typography fontSize={40}>Hello</Typography>
-                    <Typography fontSize={40}>Hello</Typography>
-                </Box>
+        <Stack height={'87vh'} direction={'column'} justifyContent={'space-between'} spacing={2} my={2} mx={2}>
+            <Paper sx={{height:'60vh', overflowY:'scroll', display:"flex", flexGrow: 1, backgroundColor:'whitesmoke'}}>
+                <Stack width={'100%'} mx={2} my={1} direction={"column"} spacing={1}>
+                    <CommentCard data={{author: 'Tejaswi', content:'Some Content', date: '13/03/2023', replies:420}}/>
+                    <CommentCard data={{author: 'Tejaswi', content:'Some Content', date: '13/03/2023'}}/>
+                </Stack>
             </Paper>
             <Paper sx={{height:100, backgroundColor:'whitesmoke'}}>
-                <TextField multiline rows={2} color="success" sx={{backgroundColor:"white", display:"flex", flexGrow: 1}}/>
+                <Stack height={'100%'} direction={"column"} justifyContent={'center'} mx={2}>
+                    <Stack direction={"row"} spacing={1}>                    
+                        <TextField label='Comment' multiline rows={2} color="success" sx={{backgroundColor:"white", display:"flex", flexGrow: 1}}/>
+                        <Button disabled={false}>
+                            <SendIcon/>
+                        </Button>
+                    </Stack>
+                </Stack>
             </Paper>
         </Stack>
     );
